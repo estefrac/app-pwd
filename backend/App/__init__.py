c18a24aae000e6626403d3336b643fd525cf853a
estefrac <estefrac@gmail.com>
2026-03-19 00:15:18 -0300

    feat: agrego tres rutas en __init__.py

--- backend/App/__init__.py
+++ backend/App/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return f'Practicando para la materia Programación Web Dinámica'
+    @app.route('/')
+    @app.route('/<nombre>')
+    def home(nombre=None):
+        if (nombre == None):
+            return f' <h1>Hola desde Programación Web Dinámica 2026<h1>'
+        return f' <h1>Hola {nombre} te saludamos desde Programación Web Dinámica<h1>'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    @app.route('/saludo')
+    def saludo():
+        return f' <h1>Saludo desde Programación Web Dinámica 2026<h1>'
+
+    return app
