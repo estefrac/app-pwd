37e2558c81faa975680437f76245c96a41e40615
estefrac <estefrac@gmail.com>
2026-03-27 17:08:49 -0300

    chore: minor style cleanup in run.py

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,3 @@
-
 from app import create_app
 from app.models import db
 from app.models.user import Usuario
@@ -6,5 +5,5 @@ from app.models.user import Usuario
 app = create_app()
 
 with app.app_context():
-    db.create_all()  # Crea las tablas en la base de datos
-    print("Tablas creadas exitosamente.")
+    db.create_all()
+    print('las tablas se crearon correctamente')
